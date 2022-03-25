b1df0fdf533dd5a5825284ad7a188b6049cfd04f
Constartine <315951259@qq.com>
2022-03-25 21:29:17 +0800

    准备提交远程库

---
+++ app/src/main/java/com/example/checklist/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.checklist.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.checklist.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
